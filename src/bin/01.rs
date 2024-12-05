97413c6a74241a3aee75319758069c1dd54f6519
tomggill <tom.gill25@outlook.com>
2024-12-05 22:30:02 +0000

    Add solution for Day 1 Part 2 2024

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,6 +1,8 @@
+use std::collections::HashMap;
+
 advent_of_code::solution!(1);
 
-pub fn part_one(input: &str) -> Option<u32> {
+fn parse_list(input: &str) -> (Vec<i32>, Vec<i32>) {
     let mut list_one: Vec<i32> = Vec::new();
     let mut list_two: Vec<i32> = Vec::new();
     for line in input.lines() {
@@ -12,6 +14,11 @@ pub fn part_one(input: &str) -> Option<u32> {
         list_one.push(row[0]);
         list_two.push(row[1]);
     }
+    (list_one, list_two)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (mut list_one, mut list_two) = parse_list(input);
     list_one.sort_unstable();
     list_two.sort_unstable();
     let total_distance: u32 = list_one
@@ -24,7 +31,16 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (list_one, list_two) = parse_list(input);
+    let mut count_map: HashMap<i32, u32> = HashMap::new();
+    for num in list_two {
+        *count_map.entry(num).or_insert(0) += 1;
+    }
+    let mut result = 0;
+    for num in list_one {
+        result += (num as u32) * count_map.get(&num).unwrap_or(&0);
+    }
+    Some(result)
 }
 #[cfg(test)]
 mod tests {
@@ -37,14 +53,8 @@ mod tests {
     }
 
     #[test]
-    fn test_part_one_input() {
-        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
-        assert_eq!(result, None);
-    }
-
-    #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(31));
     }
 }
